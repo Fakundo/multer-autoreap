21e55d9c4644de6800e4928801b126b09f09940a
Fakundo <fakundo.roman@gmail.com>
2014-06-24 03:10:03 +0400

    Added support for files with same fieldname

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ var debug = require('debug')('mutler-autoreap:middleware');
 // auto remove any uploaded files on response end
 // to persist uploaded files, simply move them to a permanent location,
 // or delete the req.files[key] before the response end.
-module.exports = function(req, res, next) {
-	var reapFiles = function reapFiles() {
-		var file;
-		if (typeof req.files === "object") {
-			for(var key in req.files) {
-				if (req.files.hasOwnProperty(key)) {
-					file = req.files[key];
-					delete req.files[key]; // avoids stating previously reaped files
-					fs.stat(file.path, function(err, stats) {
-						if (!err && stats.isFile()) {
-							fs.unlink(file.path);
-							debug('removed ' + file.path);
-							res.emit('autoreap', file);
-						}
-					});
-				}
-			}
-		}
-	};
+module.exports = function (req, res, next) {
+    var reapFiles = function reapFiles() {
+        var file;
+        if (typeof req.files === "object") {
+            for (var key in req.files) {
+                if (req.files.hasOwnProperty(key)) {
+                    file = req.files[key];
+                    delete req.files[key]; // avoids stating previously reaped files
+                    [].concat(file).forEach(function (file) {
+                        fs.stat(file.path, function (err, stats) {
+                            if (!err && stats.isFile()) {
+                                fs.unlink(file.path);
+                                debug('removed ' + file.path);
+                                res.emit('autoreap', file);
+                            }
+                        });
+                    });
+                }
+            }
+        }
+    };
 
-	res.on('finish', reapFiles);
-	res.on('close', reapFiles);
-	next();
+    res.on('finish', reapFiles);
+    res.on('close', reapFiles);
+    next();
 };
